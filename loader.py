51917e8e86709a9b559e1a9e68909399be634d6e
bebus <v.gerashchenkoua@gmail.com>
2024-01-10 15:12:18 +0200

    Initial commit

---
+++ loader.py
@@ -0,0 +1,6 @@
+import logging
+from aiogram import Bot, Dispatcher
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+dp = Dispatcher()
---
+++ main.py
@@ -0,0 +1,9 @@
+import asyncio
+from loader import dp, bot
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
